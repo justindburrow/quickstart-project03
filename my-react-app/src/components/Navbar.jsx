be737c4fb4c287b65c95a96fa50f3d9f17adb84c
justindburrow <justinburrow@Justins-MacBook-Pro.local>
2025-05-05 09:50:44 -0500

    added working branch for edits

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/todos">Todos</Link>
+      <Link to="/todos">Todo List</Link>
       <Link to="/contact">Contact</Link>
     </nav>
   );
